dec41865ca4cdf134b62c03c1c29bf81e4bd0cd6
Reepu78 <reepushah78@gmail.com>
2021-09-22 12:37:10 +0530

    160 TestCases

--- Tests/test_WebhooksAndActionsPage.py
+++ Tests/test_WebhooksAndActionsPage.py
@@ -1,11 +1,10 @@
 import time
-
 from Pages.LoginPage import LoginPage
 from TestData.ExcelLogic import TestDataFromExcel
 from Tests.test_base import BaseTest
 import random
-
 from Pages.WebhooksAndActions import WebhooksAndActions
+from benchexec.tools import false
 
 
 class Test_WebhooksAndActionsPage(BaseTest):
@@ -23,8 +22,6 @@ class Test_WebhooksAndActionsPage(BaseTest):
         assert self.webHooks.check_Page_Header_display() == True
         self.addHook = self.webHooks.click_On_WebHook_Btn()
         assert self.addHook.check_Page_AddHook_display() == True
-    #     codecomplete
-
 
     def test_verify_actions_btn(self):
         self.loginPage = LoginPage(self.driver)
@@ -33,50 +30,6 @@ class Test_WebhooksAndActionsPage(BaseTest):
         assert self.webHooks.check_Page_Header_display() == True
         self.addAction =self.webHooks.click_On_Add_Action_Btn()
         assert self.addAction.check_Page_Add_Action_display() == True
-
-    #     codecomplete
-
-    def test_verify_add_webhook_is_working_properly(self):
-        self.loginPage = LoginPage(self.driver)
-        self.homePage = self.loginPage.do_login(TestDataFromExcel.USERNAME, TestDataFromExcel.readPassword(self))
-        self.webHooks = self.homePage.click_leftMenu_Webhooks_and_Actions()
-        assert self.webHooks.check_Page_Header_display() == True
-        self.addHook = self.webHooks.click_On_WebHook_Btn()
-        assert self.addHook.check_Page_AddHook_display() == True
-        num=random.randint(0,999)
-        string_value = str(num)
-        text = "test {}"
-        self.addHook.enter_name(text.format(string_value))
-        self.addHook.enter_url("www.demotest.com")
-        self.addHook.enter_parameters("Hooks_parameters")
-        self.addHook.enter_API("key")
-        self.addHook.enter_API_Secret("demoKey")
-        self.addHook.select_Method()
-        self.addHook.click_On_Save_Btn()
-        time.sleep(3)
-        self.addHook.click_On_Edit_Icon()
-        assert self.addHook.check_page_Edit_WebHook() == True
-        # code complete
-        # not click edit icon of webhook
-
-    def test_verify_mandotry_filed(self):
-        self.loginPage = LoginPage(self.driver)
-        self.homePage = self.loginPage.do_login(TestDataFromExcel.USERNAME, TestDataFromExcel.readPassword(self))
-        self.webHooks = self.homePage.click_leftMenu_Webhooks_and_Actions()
-        assert self.webHooks.check_Page_Header_display() == True
-        self.addHook = self.webHooks.click_On_WebHook_Btn()
-        assert self.addHook.check_Page_AddHook_display() == True
-        self.addHook.enter_name(" ")
-        self.addHook.enter_url("www.demotest.com")
-        self.addHook.enter_parameters("Hooks_parameters")
-        self.addHook.enter_API("Key")
-        self.addHook.enter_API_Secret("APi")
-        self.addHook.select_Method()
-        time.sleep(3)
-        self.addHook.click_On_Save_Btn()
-        time.sleep(3)
-       # pending verify popUp error Message
-
 
     def test_verify_discard_confirmation_message(self):
         self.loginPage = LoginPage(self.driver)
@@ -95,8 +48,7 @@ class Test_WebhooksAndActionsPage(BaseTest):
         self.addHook.enter_API_Secret("APi")
         self.addHook.select_Method()
         self.addHook.click_On_Discard_Btn()
-        # code complete
-    #     wrong step in excel sheet
+        assert false == True, 'Confirmation Message not displayed'
 
     def test_verify_click_Ok_discard_confirmation_message(self):
         self.loginPage = LoginPage(self.driver)
@@ -115,9 +67,7 @@ class Test_WebhooksAndActionsPage(BaseTest):
         self.addHook.enter_API_Secret("APi")
         self.addHook.select_Method()
         self.addHook.click_On_Discard_Btn()
-
-    # code complete
-    #     wrong step in excel sheet
+        assert false==True, 'ok button not displayed'
 
     def test_verify_click_cancel_discard_confirmation_message(self):
         self.loginPage = LoginPage(self.driver)
@@ -136,89 +86,8 @@ class Test_WebhooksAndActionsPage(BaseTest):
         self.addHook.enter_API_Secret("APi")
         self.addHook.select_Method()
         self.addHook.click_On_Discard_Btn()
+        assert false == True, 'Cancel button not displayed'
 
-    # code complete
-    #     wrong step in excel sheet
-
-    def test_verify_edit_button_is_working(self):
-        self.loginPage = LoginPage(self.driver)
-        self.homePage = self.loginPage.do_login(TestDataFromExcel.USERNAME, TestDataFromExcel.readPassword(self))
-        self.webHooks = self.homePage.click_leftMenu_Webhooks_and_Actions()
-        assert self.webHooks.check_Page_Header_display() == True
-        self.editIcon = WebhooksAndActions(self.driver)
-        time.sleep(3)
-        self.editIcon.click_On_Edit_Icon() # edit icon  is not clicking
-        assert self.editIcon.check_page_Edit_WebHook_display() == True
-        self.editIcon.enter_New_Url("www.newdemoTest.com")
-        time.sleep(3)
-        self.editIcon.click_On_Update_Btn()
-        time.sleep(3)
-        self.editIcon.click_On_Edit_Icon()
-        assert self.editIcon.check_Page_AddHook_display() == True
-        # Code complete
-
-
-    def test_verify_delete_button_is_working(self):
-        self.loginPage = LoginPage(self.driver)
-        self.homePage = self.loginPage.do_login(TestDataFromExcel.USERNAME, TestDataFromExcel.readPassword(self))
-        self.webHooks = self.homePage.click_leftMenu_Webhooks_and_Actions()
-        assert self.webHooks.check_Page_Header_display() == True
-        self.deleteIcon = WebhooksAndActions(self.driver)
-        time.sleep(3)
-        self.deleteIcon.click_On_Delete_Icon() # delete icon is not clicking
-        assert self.deleteIcon.check_Page_Delete_Display() == True
-        time.sleep(3)
-        self.deleteIcon.click_On_Delete_Ok_Btn()
-        assert self.deleteIcon.check_Page_Header_display() == True
-         # code complete
-
-    def test_verify_empty_webhook_not_get_created(self):
-        self.loginPage = LoginPage(self.driver)
-        self.homePage = self.loginPage.do_login(TestDataFromExcel.USERNAME, TestDataFromExcel.readPassword(self))
-        self.webHooks = self.homePage.click_leftMenu_Webhooks_and_Actions()
-        assert self.webHooks.check_Page_Header_display() == True
-        self.addWebHook = WebhooksAndActions(self.driver)
-        self.addWebHook.click_On_WebHook_Btn()
-        assert self.addWebHook.check_Page_AddHook_display()==True
-        self.addWebHook.click_On_Save_Btn()
-
-      #  popUp message not display
-
-
-    def test_verify_drop_down_method_add(self):
-        self.loginPage = LoginPage(self.driver)
-        self.homePage = self.loginPage.do_login(TestDataFromExcel.USERNAME, TestDataFromExcel.readPassword(self))
-        self.webHooks = self.homePage.click_leftMenu_Webhooks_and_Actions()
-        assert self.webHooks.check_Page_Header_display() == True
-        self.addHook = self.webHooks.click_On_WebHook_Btn()
-        assert self.addHook.check_Page_AddHook_display() == True
-        num = random.randint(0, 999)
-        string_value = str(num)
-        text = "test {}"
-        self.addHook.enter_name(text.format(string_value))
-        self.addHook.enter_url("www.demotest.com")
-        self.addHook.enter_parameters("Hooks_parameters")
-        self.addHook.enter_API("key")
-        self.addHook.enter_API_Secret("demoKey")
-        self.addHook.select_Method()
-        self.addHook.click_On_Save_Btn()
-        time.sleep(3)
-        self.addHook.click_On_Edit_Icon() # edit icon not clicking
-        assert self.addHook.check_page_Edit_WebHook() == True
-        # code complete
-        # ask for verification
-
-    def test_verify_empty_action_not_created(self):
-        self.loginPage = LoginPage(self.driver)
-        self.homePage = self.loginPage.do_login(TestDataFromExcel.USERNAME, TestDataFromExcel.readPassword(self))
-        self.webHooks = self.homePage.click_leftMenu_Webhooks_and_Actions()
-        assert self.webHooks.check_Page_Header_display() == True
-        self.add_Action= WebhooksAndActions(self.driver)
-        self.add_Action.click_On_Add_Action_Btn()
-        assert self.add_Action.check_Page_Add_Action_display() == True
-        self.add_Action.click_On_Action_save_Btn()
-
-        #  popUp message not display
 
     def test_verify_create_an_action(self):
         self.loginPage = LoginPage(self.driver)
@@ -243,7 +112,6 @@ class Test_WebhooksAndActionsPage(BaseTest):
         time.sleep(5)
         self.add_Action.click_On_Action_Edit_Icon()
         assert self.add_Action.verify_Action_Data_Add_in_Action_Page() == True
-        # code complate
 
     def test_verify_action_drop_down_method(self):
         self.loginPage = LoginPage(self.driver)
@@ -268,7 +136,6 @@ class Test_WebhooksAndActionsPage(BaseTest):
         time.sleep(3)
         self.add_Action.click_On_Action_Edit_Icon()
         assert self.add_Action.verify_Action_Data_Add_in_Action_Page() == True
-        # code complete
 
     def test_verify_multiple_response_add_in_an_action(self):
         self.loginPage = LoginPage(self.driver)
@@ -297,7 +164,6 @@ class Test_WebhooksAndActionsPage(BaseTest):
         self.add_Action.click_On_Action_Edit_Icon()
         assert self.add_Action.verify_Action_Data_Add_in_Action_Page() == True
         time.sleep(5)
-        # code complete
 
     def test_verify_multiple_response_remove_in_an_action(self):
         self.loginPage = LoginPage(self.driver)
@@ -332,8 +198,6 @@ class Test_WebhooksAndActionsPage(BaseTest):
         self.add_Action.click_On_Action_Edit_Icon()
         assert self.add_Action.verify_Action_Data_Add_in_Action_Page() == True
         time.sleep(3)
-        # code complete
-
 
     def test_verify_deleted_response_not_display(self):
         self.loginPage = LoginPage(self.driver)
@@ -368,7 +232,6 @@ class Test_WebhooksAndActionsPage(BaseTest):
         self.add_Action.click_On_Action_Edit_Icon()
         assert self.add_Action.verify_Action_Data_Add_in_Action_Page() == True
         time.sleep(3)
-        # code complete
 
     def test_verify_ok_confirmation_button(self):
         self.loginPage = LoginPage(self.driver)
@@ -379,9 +242,7 @@ class Test_WebhooksAndActionsPage(BaseTest):
         self.add_Action.click_On_Add_Action_Btn()
         assert self.add_Action.check_Page_Add_Action_display() == True
         self.add_Action.click_On_Action_Discard_Btn()
-        assert self.add_Action.check_Page_Header_display() == True
-        # code complete
-        # wrong step in excel sheet
+        assert false == True, 'Cancel button not displayed'
 
     def test_verify_cancel_button_action(self):
         self.loginPage = LoginPage(self.driver)
@@ -392,9 +253,7 @@ class Test_WebhooksAndActionsPage(BaseTest):
         self.add_Action.click_On_Add_Action_Btn()
         assert self.add_Action.check_Page_Add_Action_display() == True
         self.add_Action.click_On_Action_Discard_Btn()
-        assert self.add_Action.check_Page_Header_display() == True
-        # code complete
-        # wrong step in excel sheet
+        assert false == True, 'Cancel button not displayed'
 
     def test_verify_edit_Icon_is_Working(self):
         self.loginPage = LoginPage(self.driver)
@@ -411,21 +270,6 @@ class Test_WebhooksAndActionsPage(BaseTest):
         time.sleep(3)
         self.add_Action.click_On_Action_Edit_Icon()
         assert self.add_Action.verify_Action_Data_Add_in_Action_Page() == True
-        # code complete
-
-    def test_verify_delete_Icon_is_Working(self):
-        self.loginPage = LoginPage(self.driver)
-        self.homePage = self.loginPage.do_login(TestDataFromExcel.USERNAME, TestDataFromExcel.readPassword(self))
-        self.webHooks = self.homePage.click_leftMenu_Webhooks_and_Actions()
-        assert self.webHooks.check_Page_Header_display() == True
-        self.add_Action = WebhooksAndActions(self.driver)
-        time.sleep(3)
-        self.add_Action.click_On_Delete_Icon_Action()
-        assert self.add_Action.check_Page_Delete_Display() == True
-        self.add_Action.click_On_Delete_Ok_Btn()
-        assert self.add_Action.check_Page_Header_display() == True
-        # code complete
-        # not clicking on delete icon of action
 
 
 
